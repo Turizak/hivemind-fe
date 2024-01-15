1bfaafe3e2deeb90340de8b3f18e77bc6c39db0b
Tom Slanda <tomslanda@Toms-MacBook-Air.local>
2024-01-14 19:33:26 -0500

    Added radix navigation bar and icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import Navbar from "./components/Navbar"
+
 function App() {
 
   return (
     <>
-      <h1 className="flex justify-center text-5xl font-bold">The Future Home of Hivemind</h1>
+    <Navbar />
+      <h1 className="flex text-center text-4xl font-bold p-3">The Future Home of Hivemind</h1>
       </>
   )
 }
