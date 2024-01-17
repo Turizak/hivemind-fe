6120cf71932695880db93df8ea999b8536fa73c2
Tom Slanda <tomslanda@Toms-MacBook-Air.local>
2024-01-16 19:35:28 -0500

    messed up

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar"
 
+import ContentContainer from "./ContentContainer"
 function App() {
 
   return (
     <>
     <Navbar />
       <h1 className="flex text-center text-4xl font-bold p-3">The Future Home of Hivemind</h1>
+      <ContentContainer />
       </>
   )
 }
