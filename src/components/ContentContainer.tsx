bce790625afacded9d67bc4e85715844edb795f2
Tom Slanda <tomslanda@Toms-MacBook-Air.local>
2024-01-17 19:11:55 -0500

    added time container and filler text to content container

---
+++ src/components/ContentContainer.tsx
@@ -0,0 +1,11 @@
+const ContentContainer = () => {
+  return (
+    <div className="p-3 bg-gray-500">
+      <div className="flex w-max p-2 rounded-md bg-gray-400 text-sm">
+        <p>15 hrs ago</p>
+      </div>
+      </div>
+  )
+}
+
+export default ContentContainer
