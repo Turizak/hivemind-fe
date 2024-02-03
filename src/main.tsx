8c938cd745d07c6e0dcae8371fa31c3c23d3459f
Tom Slanda <tomslanda@Toms-MacBook-Air.local>
2024-02-03 09:21:51 -0500

    installed and configured react router 6

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App.tsx'
 import "./index.css"
 
+
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
