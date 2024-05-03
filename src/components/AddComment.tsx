580471ed14382db2fdd3f7def42d9ddf072b58db
Robert Kazirut <robert.kazirut@gmail.com>
2024-05-03 11:00:30 -0400

    rk hailmary

--- src/components/AddComment.tsx
+++ src/components/AddComment.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useMutation } from "@tanstack/react-query";
 
-// AddComment component 
 const AddComment = (props: any) => {
   const [disabled, setDisabled] = useState<boolean>(false);
   const [buttonText, setButtonText] = useState<string>("Add Comment");
