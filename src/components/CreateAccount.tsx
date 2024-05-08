fbbd3fea0d46eece14f5aaf462667c0bc7652580
Tom Slanda <tomslanda@Toms-MacBook-Air.local>
2024-05-07 22:38:42 -0400

    form works

--- src/components/CreateAccount.tsx
+++ src/components/CreateAccount.tsx
@@ -1,8 +1,63 @@
 import * as Form from "@radix-ui/react-form";
+import { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
 
-const CreateAccount = () => (
+type Account = {
+  username: string;
+  password: string;
+  email: string;
+}
+
+const CreateAccount = () => {
+  const [buttonText, setButtonText] = useState<string>("Submit")
+  const [disabled, setDisabled] = useState<boolean>(false);
+  const usernameRef = useRef<HTMLInputElement>(null)
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const baseURL = import.meta.env.VITE_BASEURL;
+  const navigate = useNavigate();
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    setDisabled(true);
+    const accountObject: Account = {
+      username: `${usernameRef?.current?.value}`,
+      password: `${passwordRef?.current?.value}`,
+      email: `${emailRef?.current?.value}`,
+    };
+    mutate(accountObject);
+  }
+  const { mutate } = useMutation({
+    mutationFn: login,
+  });
+
+  async function login(body: Account) {
+    try {
+      const response = await fetch(baseURL + "/account/create", {
+        method: "POST",
+        headers: {
+          Accept: "*/*",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+      if (!response.ok) {
+        setDisabled(false)
+        throw new Error(`${response.status}`);
+      }
+      const results = await response.json();
+      setButtonText("Account Created!")
+      setTimeout(() => {
+        navigate("/");
+      }, 1500);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+return (
   <div className="flex justify-center">
-    <Form.Root className="w-[260px]">
+    <Form.Root className="w-[260px]" onSubmit={handleSubmit}>
       <Form.Field className="grid mb-[10px]" name="username">
         <div className="flex items-baseline justify-between">
           <Form.Label className="text-[15px] font-medium leading-[35px] text-black">
@@ -25,6 +80,8 @@ const CreateAccount = () => (
           <input
             className="box-border w-full bg-blackA2 shadow-blackA6 inline-flex h-[35px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none text-black shadow-[0_0_0_1px] outline-none hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] selection:color-white selection:bg-blackA6"
             type="username"
+            disabled={disabled}
+            ref={usernameRef}
             required
           />
         </Form.Control>
@@ -44,13 +101,15 @@ const CreateAccount = () => (
             className="text-[13px] text-black opacity-[0.8]"
             match="typeMismatch"
           >
-            Please provide a valid password
+            Password Mismatch
           </Form.Message>
         </div>
         <Form.Control asChild>
           <input
             className="box-border w-full bg-blackA2 shadow-blackA6 inline-flex h-[35px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none text-black shadow-[0_0_0_1px] outline-none hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] selection:color-white selection:bg-blackA6"
             type="password"
+            ref={passwordRef}
+            disabled={disabled}
             required
           />
         </Form.Control>
@@ -64,19 +123,20 @@ const CreateAccount = () => (
             className="text-[13px] text-black opacity-[0.8]"
             match="valueMissing"
           >
-            Please confirm your password
+            Please confirm
           </Form.Message>
           <Form.Message
             className="text-[13px] text-black opacity-[0.8]"
             match="typeMismatch"
           >
-            Passwords do not match
+            Password Mismatch
           </Form.Message>
         </div>
         <Form.Control asChild>
           <input
             className="box-border w-full bg-blackA2 shadow-blackA6 inline-flex h-[35px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none text-black shadow-[0_0_0_1px] outline-none hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] selection:color-white selection:bg-blackA6"
             type="password"
+            disabled={disabled}
             required
           />
         </Form.Control>
@@ -103,17 +163,20 @@ const CreateAccount = () => (
           <input
             className="box-border w-full bg-blackA2 shadow-blackA6 inline-flex h-[35px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none text-black shadow-[0_0_0_1px] outline-none hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] selection:color-white selection:bg-blackA6"
             type="email"
+            ref={emailRef}
+            disabled={disabled}
             required
           />
         </Form.Control>
       </Form.Field>
       <Form.Submit asChild>
         <button className="box-border w-full text-white shadow-blackA4 inline-flex h-[35px] items-center justify-center rounded-[4px] bg-black px-[15px] font-medium leading-none shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none mt-[10px]">
-          Create Account
+          {buttonText}
         </button>
       </Form.Submit>
     </Form.Root>
   </div>
-);
+)
+};
 
 export default CreateAccount;
