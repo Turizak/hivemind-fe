95afc21fc495c816f270d2b4346173b565419345
Tom Slanda <tomslanda@Toms-Laptop-2.wowway.com>
2024-05-16 10:24:25 -0400

    login form working

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,5 +1,5 @@
-import * as Form from "@radix-ui/react-form";
-import { useRef, useState, useContext } from "react";
+import { useContext } from "react";
+import { useForm } from "@tanstack/react-form";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import SessionContext from "../context/SessionProvider";
@@ -13,27 +13,23 @@ type LoginCredentials = {
 };
 
 const LoginForm: React.FC = () => {
-  const [disabled, setDisabled] = useState<boolean>(false);
-  const emailRef = useRef<HTMLInputElement>(null);
-  const passwordRef = useRef<HTMLInputElement>(null);
   const baseURL = import.meta.env.VITE_BASEURL;
   const navigate = useNavigate();
   const setError = useSetError();
 
+  const form = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: async ({ value }) => {
+      login(value);
+    },
+  });
+
   //@ts-expect-error
   // Session hook
   const { session, setSession } = useContext(SessionContext);
-
-  // Login
-  function handleSubmit(e: React.FormEvent) {
-    e.preventDefault();
-    setDisabled(true);
-    const loginObject: LoginCredentials = {
-      email: `${emailRef?.current?.value}`,
-      password: `${passwordRef?.current?.value}`,
-    };
-    login(loginObject);
-  }
 
   async function login(body: LoginCredentials) {
     try {
@@ -46,7 +42,6 @@ const LoginForm: React.FC = () => {
         body: JSON.stringify(body),
       });
       if (!response.ok) {
-        setDisabled(false);
         throw new Error(`${response.status}`);
       }
       const results = await response.json();
@@ -70,74 +65,64 @@ const LoginForm: React.FC = () => {
   return (
     <>
       <div className="flex justify-center">
-        <Form.Root className="w-[260px]" onSubmit={handleSubmit}>
-          <Form.Field className="grid mb-[10px]" name="email">
-            <div className="flex items-baseline justify-between">
-              <Form.Label className="text-[15px] font-medium leading-[35px] text-black">
-                Email
-              </Form.Label>
-              <Form.Message
-                className="text-[13px] text-red-500"
-                match="valueMissing"
-              >
-                Please enter your email
-              </Form.Message>
-              <Form.Message
-                className="text-[13px] text-red-500"
-                match="typeMismatch"
-              >
-                Please provide a valid email
-              </Form.Message>
-            </div>
-            <Form.Control asChild>
-              <input
-                className="box-border w-full bg-blackA2 shadow-blackA6 inline-flex h-[35px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none text-black shadow-[0_0_0_1px] outline-none hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] selection:color-white selection:bg-blackA6"
-                type="email"
-                ref={emailRef}
-                disabled={disabled}
-                required
-              />
-            </Form.Control>
-          </Form.Field>
-          <Form.Field className="grid mb-[10px]" name="password">
-            <div className="flex items-baseline justify-between">
-              <Form.Label className="text-[15px] font-medium leading-[35px] text-black">
-                Password
-              </Form.Label>
-              <Form.Message
-                className="text-[13px] text-red-500"
-                match="valueMissing"
-              >
-                Please enter your password
-              </Form.Message>
-              <Form.Message
-                className="text-[13px] text-red-500"
-                match="typeMismatch"
-              >
-                Please provide a valid password
-              </Form.Message>
-            </div>
-            <Form.Control asChild>
-              <input
-                className="box-border w-full bg-blackA2 shadow-blackA6 inline-flex h-[35px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none text-black shadow-[0_0_0_1px] outline-none hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] selection:color-white selection:bg-blackA6"
-                type="password"
-                ref={passwordRef}
-                disabled={disabled}
-                required
-              />
-            </Form.Control>
-          </Form.Field>
-          <Form.Submit asChild>
-            <button className="box-border w-full text-white shadow-blackA4 inline-flex h-[35px] items-center justify-center rounded-[4px] bg-black px-[15px] font-medium leading-none shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none mt-[10px]">
-              Login
-            </button>
-          </Form.Submit>
+        <form
+          className="w-[260px]"
+          onSubmit={(e) => {
+            e.preventDefault();
+            form.handleSubmit();
+          }}
+        >
+          <div className="flex items-baseline justify-between">
+            <label
+              htmlFor="email"
+              className="text-[15px] font-medium leading-[35px] text-black"
+            >
+              Email
+            </label>
+            <form.Field
+              name="email"
+              children={(email) => (
+                <input
+                  name="email"
+                  type="email"
+                  className="box-border w-full bg-blackA2 shadow-blackA6 inline-flex h-[35px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none text-black shadow-[0_0_0_1px] outline-none hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] selection:color-white selection:bg-blackA6"
+                  value={email.state.value}
+                  onChange={(e) => email.handleChange(e.target.value)}
+                  required
+                />
+              )}
+            />
+          </div>
+          <div className="flex items-baseline justify-between">
+            <label className="text-[15px] font-medium leading-[35px] text-black">
+              Password
+            </label>
+            <form.Field
+              name="password"
+              children={(password) => (
+                <input
+                  name="password"
+                  className="box-border w-full bg-blackA2 shadow-blackA6 inline-flex h-[35px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none text-black shadow-[0_0_0_1px] outline-none hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] selection:color-white selection:bg-blackA6"
+                  type="password"
+                  value={password.state.value}
+                  onChange={(e) => password.handleChange(e.target.value)}
+                  required
+                />
+              )}
+            />
+          </div>
+          <button
+            type="submit"
+            className="box-border w-full text-white shadow-blackA4 inline-flex h-[35px] items-center justify-center rounded-[4px] bg-black px-[15px] font-medium leading-none shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none mt-[10px]"
+          >
+            Login
+          </button>
           <Link to="/createAccount">
             <button className="box-border w-full text-white shadow-blackA4 inline-flex h-[35px] items-center justify-center rounded-[4px] bg-black px-[15px] font-medium leading-none shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none mt-[20px]">
               Create Account
             </button>
           </Link>
-        </Form.Root>
+        </form>
       </div>
     </>
   );
