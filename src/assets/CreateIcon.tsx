613cd7d1cd04bdbd25133d4f616b7a56fb187c8c
Tom Slanda <tomslanda@Toms-MacBook-Air.local>
2024-08-03 14:19:16 -0400

    missed a couple of icons

--- src/assets/CreateIcon.tsx
+++ src/assets/CreateIcon.tsx
@@ -1,12 +1,23 @@
-const CreateIcon = () => {
+import { SvgProps } from "../types";
+
+const CreateIcon: React.FC<SvgProps> = ({
+  xmlns = 'http://www.w3.org/2000/svg',
+  viewBox = '0 0 24 24',
+  fill = 'none',
+  strokeWidth = 2,
+  stroke = 'currentColor',
+  className = 'w-6 h-6',
+  ...props
+}) => {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="w-6 h-6"
+      xmlns={xmlns}
+      viewBox={viewBox}
+      fill={fill}
+      strokeWidth={strokeWidth}
+      stroke={stroke}
+      className={className}
+      {...props}
     >
       <path
         strokeLinecap="round"
