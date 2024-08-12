6e05acb7a031fa9330977226310f491d95eb201d
Tom Slanda <tomslanda@Toms-Laptop-68.local>
2024-08-12 16:34:10 -0400

    reverted useState

--- src/components/VoteContainer.tsx
+++ src/components/VoteContainer.tsx
@@ -1,42 +1,33 @@
-import { useState } from "react";
 import useShowVotes from "../hooks/useShowVotes";
 import UpvoteIcon from "../assets/UpvoteIcon";
 import DownvoteIcon from "../assets/DownvoteIcon";
 
 const VoteContainer = (item: any) => {
-  const [hidden, setHidden] = useState("hidden ");
   const votingState = useShowVotes(item.Uuid);
-
-  function clickHandler() {}
+  console.log(votingState);
 
   return (
     <>
       {votingState.upvote === true ? (
         <>
-          <button
-            className="block hover:cursor-pointer"
-            onClick={() => (votingState.upvote = false)}
-          >
-            <UpvoteIcon
-              fill="rgba(251, 191, 36, 1)"
-              stroke="rgba(0, 0, 0, 1)"
-            />
+          <button className="block hover:cursor-pointer">
+            <UpvoteIcon fill="rgba(251,191,36,1)" stroke="rgba(0, 0, 0, 1)" />
           </button>
           <p className="p-2">{item.Upvote}</p>
         </>
-      ) : votingState.downvote === false && votingState.upvote === false ? (
+      ) : votingState.upvote === false && votingState.downvote === false ? (
         <>
           <button className="block hover:cursor-pointer">
-            <UpvoteIcon />
+            <UpvoteIcon fill="rgba(0,0,0,1)" stroke="rgba(0, 0, 0, 1)" />
           </button>
           <p className="p-2">{item.Upvote}</p>
         </>
       ) : (
         <>
-          <button className={hidden + "block hover:cursor-pointer"}>
+          <button className={"hidden block hover:cursor-pointer"}>
             <UpvoteIcon />
           </button>
-          <p className={hidden + "p-2"}>{item.Upvote}</p>
+          <p className={"hidden p-2"}>{item.Upvote}</p>
         </>
       )}
       {votingState.downvote === true ? (
@@ -58,10 +49,10 @@ const VoteContainer = (item: any) => {
         </>
       ) : (
         <>
-          <button className={hidden + "block hover:cursor-pointer"}>
+          <button className={"hidden block hover:cursor-pointer"}>
             <DownvoteIcon />
           </button>
-          <p className={hidden + "p-2"}>{item.Downvote}</p>
+          <p className={"hidden p-2"}>{item.Downvote}</p>
         </>
       )}
     </>
