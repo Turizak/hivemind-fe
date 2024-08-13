8b33c87a74ad0fe165925a05cb0572dea3c3acb0
Tom Slanda <tomslanda@Toms-Laptop-68.local>
2024-08-12 23:40:24 -0400

    halfway there

--- src/components/VoteContainer.tsx
+++ src/components/VoteContainer.tsx
@@ -1,60 +1,106 @@
-import useShowVotes from "../hooks/useShowVotes";
+import { useState, useEffect } from "react";
 import UpvoteIcon from "../assets/UpvoteIcon";
 import DownvoteIcon from "../assets/DownvoteIcon";
 
 const VoteContainer = (item: any) => {
-  const votingState = useShowVotes(item.Uuid);
-  console.log(votingState);
+  const [votingState, setVotingState] = useState({
+    upvoteState: false,
+    downvoteState: false,
+    upvoteCount: item.Upvote,
+    downvoteCount: item.Downvote,
+    upvoteIconDisplay: "",
+    downvoteIconDisplay: "",
+    upvoteIconFill: "rgba(0, 0, 0, 1)",
+    downvoteIconFill: "rgba(0, 0, 0, 1)",
+    upvoteIconStroke: "none",
+    downvoteIconStroke: "none",
+  });
+
+  function renderVotingContainer(Uuid: any) {
+    const upvotes = localStorage.getItem("Upvotes");
+    const downvotes = localStorage.getItem("Downvotes");
+    if (upvotes?.includes(Uuid)) {
+      votingState.upvoteState = true;
+    }
+    if (downvotes?.includes(Uuid)) {
+      votingState.downvoteState = true;
+    }
+    if (votingState.upvoteState === true) {
+      setVotingState((votingState) => ({
+        ...votingState,
+        upvoteIconFill: "rgba(251,191,36,1)",
+        upvoteIconStroke: "rgba(0, 0, 0, 1)",
+        downvoteIconDisplay: "hidden ",
+      }));
+    }
+    if (votingState.downvoteState === true) {
+      setVotingState((votingState) => ({
+        ...votingState,
+        downvoteIconFill: "rgba(251,191,36,1)",
+        downvoteIconStroke: "rgba(0, 0, 0, 1)",
+        upvoteIconDisplay: "hidden ",
+      }));
+    }
+  }
+
+  useEffect(() => {
+    renderVotingContainer(item.Uuid);
+  }, []);
+
+  function upvoteClickHandler() {
+    setVotingState((votingState) => ({
+      ...votingState,
+      upvoteState: !votingState.upvoteState,
+    }));
+    console.log(votingState.upvoteState);
+    if (votingState.upvoteState === true) {
+      setVotingState((votingState) => ({
+        ...votingState,
+        upvoteIconFill: "rgba(251,191,36,1)",
+        upvoteIconStroke: "rgba(0, 0, 0, 1)",
+        upvoteCount: votingState.upvoteCount + 1,
+        downvoteIconDisplay: "hidden ",
+      }));
+    }
+    if (votingState.upvoteState === false) {
+      setVotingState((votingState) => ({
+        ...votingState,
+        upvoteIconFill: "rgba(0,0,0,1)",
+        upvoteIconStroke: "none",
+        upvoteCount: votingState.upvoteCount - 1,
+        downvoteIconDisplay: "",
+      }));
+    }
+  }
+  function downvoteClickHandler() {}
 
   return (
     <>
-      {votingState.upvote === true ? (
-        <>
-          <button className="block hover:cursor-pointer">
-            <UpvoteIcon fill="rgba(251,191,36,1)" stroke="rgba(0, 0, 0, 1)" />
-          </button>
-          <p className="p-2">{item.Upvote}</p>
-        </>
-      ) : votingState.upvote === false && votingState.downvote === false ? (
-        <>
-          <button className="block hover:cursor-pointer">
-            <UpvoteIcon fill="rgba(0,0,0,1)" stroke="rgba(0, 0, 0, 1)" />
-          </button>
-          <p className="p-2">{item.Upvote}</p>
-        </>
-      ) : (
-        <>
-          <button className={"hidden block hover:cursor-pointer"}>
-            <UpvoteIcon />
-          </button>
-          <p className={"hidden p-2"}>{item.Upvote}</p>
-        </>
-      )}
-      {votingState.downvote === true ? (
-        <>
-          <button className="block hover:cursor-pointer">
-            <DownvoteIcon
-              fill="rgba(251, 191, 36, 1)"
-              stroke="rgba(0, 0, 0, 1)"
-            />
-          </button>
-          <p className="p-2">{item.Downvote}</p>
-        </>
-      ) : votingState.downvote === false && votingState.upvote === false ? (
-        <>
-          <button className="block hover:cursor-pointer">
-            <DownvoteIcon />
-          </button>
-          <p className="p-2">{item.Downvote}</p>
-        </>
-      ) : (
-        <>
-          <button className={"hidden block hover:cursor-pointer"}>
-            <DownvoteIcon />
-          </button>
-          <p className={"hidden p-2"}>{item.Downvote}</p>
-        </>
-      )}
+      <button
+        className={votingState.upvoteIconDisplay + "block hover:cursor-pointer"}
+        onClick={upvoteClickHandler}
+      >
+        <UpvoteIcon
+          fill={votingState.upvoteIconFill}
+          stroke={votingState.upvoteIconStroke}
+        />
+      </button>
+      <p className={votingState.upvoteIconDisplay + "p-2"}>
+        {votingState.upvoteCount}
+      </p>
+      <button
+        className={
+          votingState.downvoteIconDisplay + "block hover:cursor-pointer"
+        }
+      >
+        <DownvoteIcon
+          fill={votingState.downvoteIconFill}
+          stroke={votingState.downvoteIconStroke}
+        />
+      </button>
+      <p className={votingState.downvoteIconDisplay + "p-2"}>
+        {votingState.downvoteCount}
+      </p>
     </>
   );
 };
--- src/hooks/useShowVotes.tsx
+++ src/hooks/useShowVotes.tsx
@@ -1,17 +1,25 @@
 const useShowVotes = (Uuid: string) => {
-    const votingState = {
-      upvote: false,
-      downvote: false,
-    };
-    const upvotes = localStorage.getItem("Upvotes");
-    const downvotes = localStorage.getItem("Downvotes");
-    if (upvotes?.includes(Uuid)) {
-      votingState.upvote = true;
-    }
-    if (downvotes?.includes(Uuid)) {
-      votingState.downvote = true;
-    }
-    return votingState;
+  const votingState = {
+    upvoteState: false,
+    downvoteState: false,
+    upvoteCount: localStorage.getItem("Upvotes"),
+    downvoteCount: localStorage.getItem("Downvotes"),
+    upvoteIconDisplay: true,
+    downvoteIconDisplay: false,
+    upvoteIconFill: "rgba(0, 0, 0, 1)",
+    downvoteIconFill: "rgba(0, 0, 0, 1)",
+    upvoteIconStroke: "none",
+    downvoteIconStroke: "none",
+  };
+  const upvotes = localStorage.getItem("Upvotes");
+  const downvotes = localStorage.getItem("Downvotes");
+  if (upvotes?.includes(Uuid)) {
+    votingState.upvoteState = true;
   }
+  if (downvotes?.includes(Uuid)) {
+    votingState.downvoteState = true;
+  }
+  return votingState;
+};
 
-  export default useShowVotes;
+export default useShowVotes;
