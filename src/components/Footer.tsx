a1c024213ce5945a88fa0850014d495a521053ba
Tom Slanda <jesus_slanda@yahoo.com>
2024-08-22 20:01:00 -0400

    added footer (#84)
    Co-authored-by: Tom Slanda <tomslanda@Toms-MacBook-Air.local>

---
+++ src/components/Footer.tsx
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: Apache-2.0
+
+import GitHubIcon from "../assets/GitHubIcon";
+
+const Footer = () => {
+    return (
+      <footer className="bg-black text-white py-2">
+        <div className="container mx-auto flex flex-col md:flex-row justify-evenly items-center text-center">
+          {/* Left Side */}
+          <div className="mb-4 md:mb-0">
+            <h4 className="text-lg font-bold">Turizak</h4>
+            <p className="text-sm">Copyright © 2024</p>
+            <div className="flex justify-center py-2">
+            <a href="https://github.com/Turizak"><GitHubIcon /></a>
+            </div>
+          </div>
+        </div>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
